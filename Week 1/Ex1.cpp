697bc7c7249f9a0eca8b092ccfa992d7b72f2f14
LinLi375 <lynnheracle@gmail.com>
2021-01-17 20:48:43 -0500

    Created new project. Added Ex1.cpp

---
+++ Week 1/Ex1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Circle
+{
+private:
+	double radius;
+public:
+	void setRadius(double r)
+	{
+		radius = r;
+	}
+	double clacArea()
+	{
+		return 3.14 * pow(radius, 2);
+	}
+};
+
+int main()
+{
+	Circle circle1, circle2;
+
+	circle1.setRadius(1);
+	circle2.setRadius(2.5);
+
+	//Print out the area
+	cout << "The area of circle 1 is " << circle1.clacArea() << endl;
+	cout << "The area of circle 2 is " << circle2.clacArea() << endl;
+
+	return 0;
+}
