3b990d130fa3ed6f283d85146142d64afcfac459
LinLi375 <lynnheracle@gmail.com>
2021-01-22 17:12:31 -0500

    start to main

--- Lab1/Lap_1.cpp
+++ Lab1/Lap_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Achievements
@@ -11,8 +12,8 @@ private:
 public:
 	Achievements()
 	{
-		m_title = "NONE";
-		m_description = "NONE";
+		m_title = "Defualt Empty";
+		m_description = "Defualt Empty";
 		m_scoreValue = 0;
 	}
 	
@@ -45,9 +46,9 @@ private:
 public:
 	Games()
 	{
-		m_gameName = "NONE";
-		m_publisher = "NONE";
-		m_developer = "NONE";
+		m_gameName = "Defualt Empty";
+		m_publisher = "Defualt Empty";
+		m_developer = "Defualt Empty";
 	}
 
 	Games(string gameName1, string publisher1, string developer1)
@@ -57,9 +58,9 @@ public:
 		m_developer = developer1;
 	}
 	
-	void setAchievementSize(int i)
+	void setAchievementSize(int achSize)
 	{
-		m_pAchievements = new Achievements[i];
+		m_pAchievements = new Achievements[achSize];
 	}
 	
 	void setAchievementData(string title2, string description2, int scoreValue2, int achSize)
@@ -67,17 +68,47 @@ public:
 		m_pAchievements[achSize] = Achievements(title2, description2, scoreValue2);
 	}
 
-	void setAchievementMemberSize(int achMemSize)
+	void setAchievementMemberOrdinal(int achMemOrdinal)
 	{
-		m_achievementsNum = achMemSize;
+		m_achievementsNum = achMemOrdinal;
 	}
 
 	void getAchievementData()
 	{
 		for (int i = 1; i <= m_achievementsNum; i++)
 		{
-			
+			cout << "\nThe NO." << i << "Achievement is" << m_pAchievements[i - 1].getAchievementsData() << endl;
 		}
+	}
+
+	int getAchMemberOrdinal()
+	{
+		return m_achievementsNum;
+	}
+
+	string getGameName()
+	{
+		return m_gameName;
+	}
+
+	string getPublisher()
+	{
+		return m_publisher;
+	}
+
+	string getGameInfo()
+	{
+		string data = "\nThe name of Game: " + m_gameName + "\n";
+		data += "\nThe name of Publisher: " + m_publisher + "\n";
+		data += "\nThe name of Developer name: " + m_developer + "\n";
+		data += "\n";
+		return data;
+	}
+
+	void deleteAchArray()
+	{
+		delete[] m_pAchievements;
+		m_pAchievements = nullptr;
 	}
 };
 
@@ -85,24 +116,116 @@ public:
 class Platform
 {
 private:
-	string m_name;
+	string m_platformName;
 	string m_manufacturer;
-	Games* m_pGames;
+	Games* m_pGames = nullptr;
 
 public:
 	Platform()
 	{
-		m_name = "NONE";
-		m_manufacturer = "NONE";
+		m_platformName = "Your Console";
+		m_manufacturer = "Default Empty";
+	}
+
+	Platform (string platformName1, string manufacturer1)
+	{
+		m_platformName = platformName1;
+		m_manufacturer = manufacturer1;
+	}
+
+	void setPlatformName (string platName)
+	{
+		m_platformName = platName;
+	}
+
+	void setManufacturer (string manufacName)
+	{
+		m_manufacturer = manufacName;
+	}
+
+	void setGamesSize (int gamesNum)
+	{
+		m_pGames = new Games[gamesNum];
+	}
+
+	void setAchievementsSize(int gamesNum, int achSize)
+	{
+		m_pGames[gamesNum].setAchievementMemberOrdinal(achSize);
+	}
+
+	void setGamesInfo (string gamesName3, string publisher3, string developer3, int achNum3)
+	{
+		m_pGames[achNum3] = Games(gamesName3, publisher3, developer3);
 	}
 	
-	void setPlatform(int j)
+	void setAchievementInfo(
+		string title4, 
+		string description4, 
+		int scoreValue4, 
+		int gameSize4, int achNum4)
 	{
-		m_pGames = new Games[j];
+		m_pGames[gameSize4].setAchievementData(title4, description4, scoreValue4, achNum4);
 	}
-	string getPlatformData()
+
+	void setAchievementOrdinal(int gamesSize5, int achMemOrdinal)
 	{
-		string data = "\nThe Games you own: "
+		m_pGames[gamesSize5].setAchievementMemberOrdinal(achMemOrdinal);
+	}
+
+	int getAchMemOrdianl(int achMemOrd1)
+	{
+		return m_pGames[achMemOrd1].getAchMemberOrdinal();
+	}
+
+	void getGamesInfo(int gamesSize)
+	{
+		cout << m_pGames[gamesSize].getGameInfo();
+	}
+	
+	void getAchievementsAllInfo(int achAllInfo)
+	{
+		m_pGames[achAllInfo].getAchievementData();
+	}
+
+	string getPlatData()
+	{
+		string data = "\nThe name of platform: " + m_platformName + "\n";
+		data += "\nThe name of manufacturer: " + m_manufacturer + "\n";
+		data += "\n";
+		return data;
+	}
+
+	string getPlatformName()
+	{
+		return m_platformName;
+	}
+
+	string getManufacturer()
+	{
+		return m_manufacturer;
+	}
+	
+	void getPlatformAllInfo(int allItemsNum)
+	{
+		cout << getPlatData() << endl;
+
+		for (int k = 1; k <= allItemsNum; k++)
+		{
+			cout << k << ") " << m_pGames[k - 1].getGameInfo() << endl;
+			m_pGames[k - 1].getAchievementData();
+		}
+	}
+
+	void delGamesArray()
+	{
+		delete[] m_pGames;
+		m_pGames = nullptr;
+	}
+
+	void delAllMem()
+	{
+		m_pGames->deleteAchArray();
+		delGamesArray();
 	}
 };
 
@@ -111,6 +234,12 @@ public:
 
 int main()
 {
+	cout << "Dear client, this is a console application for your games and achievements!\n" << endl;
 
+	cout << "You can rename your console.\n" << endl;
+
+	
+
+	
 	return 0;
 }
